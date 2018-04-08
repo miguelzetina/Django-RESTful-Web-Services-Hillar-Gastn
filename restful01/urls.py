adf5f8b87200321480d44169e819fe356ed00cf5
Miguel Angel Zetina Zetina <loefthur26@gmail.com>
2018-04-08 03:19:35 -0500

    Added tests

--- restful01/urls.py
+++ restful01/urls.py
@@ -1,20 +1,6 @@
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^v1/', include('drones.urls', namespace='v1')),
-    url(
-        r'^v1/api-auth/',
-        include(
-            'rest_framework.urls',
-            namespace='rest_framework_v1'
-        )
-    ),
-    url(r'^v2/', include('drones.v2.urls', namespace='v2')),
-    url(
-        r'^v2/api-auth/',
-        include(
-            'rest_framework.urls',
-            namespace='rest_framework_v2'
-        )
-    ),
+    url(r'^', include('drones.urls')),
+    url(r'^api-auth/', include('rest_framework.urls'))
 ]
