722b2d73cd71b2452d3cba26afba1a95a2ba222b
Miguel Angel Zetina Zetina <loefthur26@gmail.com>
2018-04-12 00:54:11 -0500

    Config Database

--- restful01/settings.py
+++ restful01/settings.py
@@ -133,3 +133,9 @@ REST_FRAMEWORK = {
     'DEFAULT_VERSIONING_CLASS':
     'rest_framework.versioning.NamespaceVersioning',
 }
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600,
+    ssl_require=True
+)
