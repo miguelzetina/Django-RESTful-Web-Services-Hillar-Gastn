4dd3377983a489b58bce8de58a77d03c07b931b0
Miguel Angel Zetina Zetina <loefthur26@gmail.com>
2019-03-11 18:27:49 -0600

    Retrieve single objects

--- drones/graphql/schema.py
+++ drones/graphql/schema.py
@@ -20,8 +20,42 @@ class Query(object):
     all_categories = graphene.List(CategoryType)
     all_drones = graphene.List(DroneType)
 
+    category = graphene.Field(
+        CategoryType, id=graphene.Int(), name=graphene.String()
+    )
+
+    drone = graphene.Field(
+        DroneType, id=graphene.Int(), name=graphene.String(),
+        manufacturing_date=graphene.types.datetime.DateTime(),
+        inserted_timestamp=graphene.types.datetime.DateTime()
+    )
+
     def resolve_all_categories(self, info, **kwargs):
         return DroneCategory.objects.all()
 
     def resolve_all_drones(self, info, **kwargs):
         return Drone.objects.select_related('drone_category').all()
+
+    def resolve_category(self, info, **kwargs):
+        id = kwargs.get('id')
+        name = kwargs.get('name')
+
+        if id is not None:
+            return DroneCategory.objects.get(pk=id)
+
+        if name is not None:
+            return DroneCategory.objects.get(name=name)
+
+        return None
+
+    def resolve_drone(self, info, **kwargs):
+        id = kwargs.get('id')
+        name = kwargs.get('name')
+
+        if id is not None:
+            return Drone.objects.get(pk=id)
+
+        if name is not None:
+            return Drone.objects.get(name=name)
+
+        return None
